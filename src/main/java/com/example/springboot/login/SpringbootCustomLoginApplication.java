996d4948d2c64e48d7b8251097676530b8e50dfd
Liton <liton.ust@gmail.com>
2021-09-02 11:17:19 +0600

    Update login process.

--- src/main/java/com/example/springboot/login/SpringbootCustomLoginApplication.java
+++ src/main/java/com/example/springboot/login/SpringbootCustomLoginApplication.java
@@ -2,9 +2,10 @@ package com.example.springboot.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootCustomLoginApplication {
+public class SpringbootCustomLoginApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCustomLoginApplication.class, args);
